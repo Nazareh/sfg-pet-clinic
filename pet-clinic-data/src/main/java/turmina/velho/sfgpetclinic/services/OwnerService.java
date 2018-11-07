35362ec68298aad8ad980339bee2a219dc5196f7
Nazareh <nazareh.turmina@tieto.com>
2018-11-07 17:23:39 +0800

    Refactoring to common crud interface. Closes #10

--- pet-clinic-data/src/main/java/turmina/velho/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/turmina/velho/sfgpetclinic/services/OwnerService.java
@@ -2,17 +2,8 @@ package turmina.velho.sfgpetclinic.services;
 
 import turmina.velho.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner,Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-
 
 }
--- pet-clinic-data/src/main/java/turmina/velho/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/turmina/velho/sfgpetclinic/services/PetService.java
@@ -2,14 +2,6 @@ package turmina.velho.sfgpetclinic.services;
 
 import turmina.velho.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet,Long>{
 
 }
