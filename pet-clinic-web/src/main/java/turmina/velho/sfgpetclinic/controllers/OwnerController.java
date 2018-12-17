d484f8b4c79e2e393be98c18539bb2ea6afe2730
Nazareh <nazareh.turmina@tieto.com>
2018-12-17 11:18:23 +0800

    Added owners List and updated controller. Closes #14,

--- pet-clinic-web/src/main/java/turmina/velho/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/turmina/velho/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package turmina.velho.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import turmina.velho.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.hmtl"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
