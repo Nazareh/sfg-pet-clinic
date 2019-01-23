fa05b70aba2b10762019c4a86d602247a77d3b48
Nazareh <nazarehturmina@gmail.com>
2019-01-23 21:48:06 +0800

    Undoing MULTI-MODULE because it was being picked up by MAVEN and implementing Aplly master layout to Index Page. Closes #17

--- pet-clinic-web/src/main/java/turmina/velho/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/turmina/velho/sfgpetclinic/SfgPetClinicApplication.java
@@ -10,4 +10,6 @@ public class SfgPetClinicApplication {
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
     }
+
+    //TODO Implement Multimodule layout
 }
