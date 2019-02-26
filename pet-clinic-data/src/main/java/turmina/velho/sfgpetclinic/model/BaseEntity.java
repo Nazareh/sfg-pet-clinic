fb44f10b446f42e2f362f3472587c340e80e1dd9
Nazareh <nazarehturmina@gmail.com>
2019-02-26 21:09:02 +0800

    Update base entity with JPA annotations. Closes #34

--- pet-clinic-data/src/main/java/turmina/velho/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/turmina/velho/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,17 @@
 package turmina.velho.sfgpetclinic.model;
 
 import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     public Long getId() {
         return id;
@@ -11,8 +20,6 @@ public class BaseEntity implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
-
-    private Long id;
 
 
 }
