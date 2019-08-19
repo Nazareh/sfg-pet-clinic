280a718aaf1df9edb9f0e0c2db4ac541a2f45805
Nazareh <nazareh.turmina@tieto.com>
2019-08-19 16:47:31 +0800

    Display Owner Record. Closes #53

--- pet-clinic-web/src/main/java/turmina/velho/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/turmina/velho/sfgpetclinic/controllers/OwnerController.java
@@ -2,7 +2,10 @@ package turmina.velho.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import turmina.velho.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
@@ -27,5 +30,12 @@ public class OwnerController {
 
         //model.addAttribute("owners",)
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
+        ModelAndView modelAndView = new ModelAndView("owners/ownerDetails");
+        modelAndView.addObject(ownerService.findById(ownerId));
+        return modelAndView;
     }
 }
