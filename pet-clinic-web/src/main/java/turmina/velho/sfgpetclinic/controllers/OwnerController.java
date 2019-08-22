d5521525e7b9c2f740df6eb4cb32e4701ec7c3f0
Nazareh <nazareh.turmina@tieto.com>
2019-08-22 15:50:27 +0800

    Create or Update Owner. Closes #55

--- pet-clinic-web/src/main/java/turmina/velho/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/turmina/velho/sfgpetclinic/controllers/OwnerController.java
@@ -4,19 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import turmina.velho.sfgpetclinic.model.Owner;
 import turmina.velho.sfgpetclinic.services.OwnerService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+
+    private final String VIEWS_OWNER_CREATE_OR_UPDATE_FORM = "owners/createOrUpdateOwnerForm";
 
     private final OwnerService ownerService;
 
@@ -30,14 +30,14 @@ public class OwnerController {
     }
 
 
-    @RequestMapping({"/find"})
+    @GetMapping({"/find"})
     public String findOwners (Model model){
 
         model.addAttribute("owner", Owner.builder().build());
         return "owners/findOwners";
     }
 
-    @RequestMapping
+    @GetMapping
     public String processFindForm(Owner owner, BindingResult result, Model model){
         //allow parameterless GET request for /owners to return all records
 
@@ -62,9 +62,50 @@ public class OwnerController {
     }
 
     @GetMapping("/{ownerId}")
-    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
+    public ModelAndView showOwner(@PathVariable Long ownerId){
         ModelAndView modelAndView = new ModelAndView("owners/ownerDetails");
         modelAndView.addObject(ownerService.findById(ownerId));
         return modelAndView;
     }
+
+    @GetMapping("/new")
+    public String initCreationForm(Model model) {
+        model.addAttribute("owner",Owner.builder().build());
+        return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+
+    }
+
+    @PostMapping("/new")
+    public String processCreationForm(@Valid Owner owner, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+        } else{
+            Owner savedOwner = ownerService.save(owner);
+            return "redirect:/owners/" + savedOwner.getId();
+        }
+
+    }
+
+    @GetMapping("/{ownerId}/edit")
+    public String initUpdateOwnerForm(@PathVariable Long ownerId, Model model) {
+
+        model.addAttribute("owner",ownerService.findById(ownerId));
+        return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+
+    }
+
+    @PostMapping("/{ownerId}/edit")
+    public String processUpdateOwnerForm(@Valid Owner owner, BindingResult result, @PathVariable Long ownerId) {
+
+        if (result.hasErrors()) {
+            return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+        } else{
+            owner.setId(ownerId);
+            Owner savedOwner = ownerService.save(owner);
+            return "redirect:/owners/" + savedOwner.getId();
+        }
+
+    }
+
 }
--- pet-clinic-web/src/main/java/turmina/velho/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/turmina/velho/sfgpetclinic/controllers/VetController.java
@@ -2,6 +2,7 @@ package turmina.velho.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import turmina.velho.sfgpetclinic.services.VetService;
 
@@ -13,7 +14,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+    @GetMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
