b5a60a7dcaf4e58f7e86b8d29550a2dd2b1c8400
nazareh <nazarehturmina@gmail.com>
2019-08-26 16:01:07 +0800

    Implements Vets JSON Endpoint. Closes #66

--- pet-clinic-web/src/main/java/turmina/velho/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/turmina/velho/sfgpetclinic/controllers/VetController.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import turmina.velho.sfgpetclinic.model.Vet;
 import turmina.velho.sfgpetclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,5 +22,11 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetJson(){
+
+        return vetService.findAll();
     }
 }
